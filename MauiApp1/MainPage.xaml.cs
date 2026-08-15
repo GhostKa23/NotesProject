293b3cff633a7a50b1acc042e02725f82ad76f48
GhostKa23 <kaiquenunes599@gmail.com>
2026-08-14 21:14:19 -0300

    Notes Project

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage() //metodo construtor
+        {
+            InitializeComponent();
+        }
+        /*
+        private void OnCounterClicked(object? sender, EventArgs e) //metodo = bloco de codigo que faz uma ação
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }*/
+
+        private void AoClicarSalvar(object? sender, EventArgs e)
+        {
+            
+        }
+
+        private void AoClicarApagar(object? sender, EventArgs e)
+        {
+
+        }
+
+        private void AoClicarCarregar(object? sender, EventArgs e)
+        {
+
+        }
+
+    }       
+}           
+            
+            
+            
+ // modificadores de acesso privado e publico || void = metodo sem retorno
